fb353be13976241d071785f1531bf7fb7dfc7b80
IvanPopoff <146200343+IvanPopoff@users.noreply.github.com>
2023-10-14 22:13:00 +0100

    Create FlyingVehicle.java
    that class inclouds Vehicle and implements the Flyable interface.

---
+++ OOCMiniHW2/src/oocminihw2/FlyingVehicle.java
@@ -0,0 +1,10 @@
+public class FlyingVehicle extends Vehicle implements Flyable {
+    @Override
+    public void changeAltitude(float change) {
+    }
+
+    @Override
+    public float getAltitude() {
+        return 0.0f; // Return a placeholder value
+    }
+}
